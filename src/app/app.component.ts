161556b9a380fd32da9be5bd21a067cdcd13ba5a
premambodkar <prem.ambodkar@gmail.com>
2022-12-28 20:40:13 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,21 @@ import { Component, VERSION, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Angular ' + VERSION.major;
-
+  menuItem: MenuItem[] = [
+    { name: 'dashboard', icon: 'home', link: '' },
+    { name: 'all timesheet', icon: 'home', link: '' },
+    { name: 'my timesheet', icon: 'home', link: '' },
+    { name: 'my projects', icon: 'home', link: '' },
+    { name: 'my profile', icon: 'home', link: '' },
+    { name: 'settings', icon: 'home', link: '' },
+  ];
   @ViewChild('mat-drawer')
   matDrawer!: any;
+}
+
+export class MenuItem {
+  name: string;
+  icon: string;
+  link: string;
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatSidenavModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+  ],
   declarations: [AppComponent, HelloComponent],
   bootstrap: [AppComponent],
 })
