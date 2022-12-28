d6ec58aa39b4dcb33244eee7f57014fbd2ce2cd2
premambodkar <prem.ambodkar@gmail.com>
2022-12-28 22:15:40 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,11 @@ import {
 export class AppComponent {
   menuItem: MenuItem[] = [
     { name: 'dashboard', icon: 'home', link: '' },
-    { name: 'all timesheet', icon: 'home', link: '' },
-    { name: 'my timesheet', icon: 'home', link: '' },
-    { name: 'my projects', icon: 'home', link: '' },
-    { name: 'my profile', icon: 'home', link: '' },
-    { name: 'settings', icon: 'home', link: '' },
+    { name: 'all timesheet', icon: 'calendar_month', link: '' },
+    { name: 'my timesheet', icon: 'schedule', link: '' },
+    { name: 'my projects', icon: 'work_history', link: '' },
+    { name: 'my profile', icon: 'groups', link: '' },
+    { name: 'settings', icon: 'tune', link: '' },
   ];
   @ViewChild('mat-drawer')
   matDrawer!: any;
